e0448f035bc39b14967968fa6d6a742d79c1173a
LaSharon Hope <123046254+lasharonhope@users.noreply.github.com>
2024-01-28 20:31:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('What is your favorite color?');
+var person = "Lily Aldrin" //Global Scope
+let life = 100 + 30 -20; //Global Scope
+
+life = life - 60; //Global Scope
+
+const car = {type:"landrover", model:"rangerover", color:"white"};
+
+
+{let x = 2;} //Block Scope
+
+console.log(person)
+console.log(life);
+console.log(car)
+console.log(x);
