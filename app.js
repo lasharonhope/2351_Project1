812b6616e88fc3c75767525adbbd500c52eaf8b6
LaSharon Hope <123046254+lasharonhope@users.noreply.github.com>
2024-02-11 23:41:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,18 @@ function toUpper(text) {
 
 toUpper(name);
 toUpper(tvshow);
+const age = 45;
+
+if (age < 34) {
+  console.log("Welcome to my page!");  
+} else if(age > 40){
+  console.log("Have you found what you were searching for?");
+}
+else {
+  console.log("What do you see?");
+}
+
+console.log(`It's almost Valentine's Day`);
+const name2 = "Chiefs";
+const name3 = "49ers";
+console.log(`The ${name2} beat the ${name3} in the SuperBowl`);
